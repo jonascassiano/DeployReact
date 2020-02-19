935daca56c60af0d64a7e4fddc929059ca3f85ad
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-02-19 01:02:46 +0100

    Updates

---
+++ precache-manifest.9d4485c46e37019330c5945741b6b9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ebe60a89469680991d36b396df39c5",
+    "url": "/DeployReact/index.html"
+  },
+  {
+    "revision": "115df9a92cea1a6e3fb3",
+    "url": "/DeployReact/static/css/main.1681de62.chunk.css"
+  },
+  {
+    "revision": "3aceaec3d70a0f94bdaa",
+    "url": "/DeployReact/static/js/2.00ece94a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/DeployReact/static/js/2.00ece94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115df9a92cea1a6e3fb3",
+    "url": "/DeployReact/static/js/main.ceef5810.chunk.js"
+  },
+  {
+    "revision": "68a9f103d6d8db3c2b3b",
+    "url": "/DeployReact/static/js/runtime-main.0328fa5f.js"
+  }
+]);
